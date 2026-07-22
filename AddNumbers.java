7471a9c2e3ec3fc11a280dc1029ce9fb42fc5116
Monisha P <monishapaulraj123@gmail.com>
2026-07-22 16:57:57 +0530

    Update AddNumbers.java

--- AddNumbers.java
+++ AddNumbers.java
@@ -2,10 +2,8 @@ import java.util.Scanner;
 
 public class AddNumbers {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int a = sc.nextInt();
-        int b = sc.nextInt();
+        int a = 10;
+        int b = 10;
 
         int sum = a + b;
 
